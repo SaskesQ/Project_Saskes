5013305b57f648d39c7ec5ddba6d30d0c2b8de24
mvazbys <mvazbys@gmail.com>
2023-03-03 15:19:38 +0200

    Help skiltis perkelta į Credits, pridėtas lentos modelis

--- app/src/main/java/com/example/saskesktu/MainActivity.java
+++ app/src/main/java/com/example/saskesktu/MainActivity.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,9 +30,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void help(View v)
+    public void credits(View v)
     {
-        Intent intent = new Intent(this, HelpActivity.class);
+        Intent intent = new Intent(this, CreditsActivity.class);
         startActivity(intent);
     }
 }
