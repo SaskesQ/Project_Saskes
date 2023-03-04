46ad5a2f2f9ceac51a078eca55e5da898c45b610
jorkvi <joris.kviesulaitis@gmail.com>
2023-03-04 12:28:21 +0200

    ar pavyko?

--- app/src/main/java/com/example/saskesktu/CreditsActivity.java
+++ app/src/main/java/com/example/saskesktu/CreditsActivity.java
@@ -2,7 +2,9 @@ package com.example.saskesktu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreditsActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class CreditsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
     }
+
+
+
+    public void grįžimas(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
