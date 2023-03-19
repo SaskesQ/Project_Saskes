bf9eff2c80980d181cef2bac8a5c070b394c9432
Matas <matkuv@ktu.lt>
2023-03-19 21:55:37 +0200

    Sukurta galimybe sustabdyti(Pause)

--- app/src/main/java/com/example/saskesktu/PlayActivity.java
+++ app/src/main/java/com/example/saskesktu/PlayActivity.java
@@ -6,12 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 public class PlayActivity extends AppCompatActivity {
@@ -51,9 +59,19 @@ public class PlayActivity extends AppCompatActivity {
         boardLogic.CheckerClicked(V, PlayActivity.this, returnBoardView(), placeId);
         BlackCapturedTextView.setText(String.valueOf(boardLogic.WhiteCaptured));
         WhiteCapturedTextView.setText(String.valueOf(boardLogic.BlackCaptured));
-
-
-
     }
-
+    public void pauseMenu(View V){
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View pauseView = inflater.inflate(R.layout.pause_menu, null);
+        final PopupWindow pauseMenu = new PopupWindow(pauseView, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, true);
+        pauseMenu.setBackgroundDrawable(new ColorDrawable());
+        pauseMenu.showAtLocation(V, Gravity.CENTER, 0, 0);
+        Button close = (Button) pauseView.findViewById(R.id.resume);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pauseMenu.dismiss();
+            }
+        });
+    }
 }
